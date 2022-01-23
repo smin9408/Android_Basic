74e1d77dfe449b01b64e02e23abbc686a6d4f002
stmdals888 <45705721+smin9408@users.noreply.github.com>
2022-01-23 20:27:04 +0900

    Kotlin Variable

---
+++ app/src/main/java/com/example/kotlin_basic/kotlin_note/01.Variable.kt
@@ -0,0 +1,24 @@
+package com.example.kotlin_basic.kotlin_note
+
+
+
+// 01. Variable 변수
+// 내 마음대로 값을 바꿀수 있는 것 -> Variable -> var
+// 한번 정하면 바꿀수 없는 것     -> Value -> val (이게 상수인듯)
+
+// 변수 선언 방법
+// var/val (변수명) = 값
+
+var num = 10
+var hello = "hello"
+var point = 167.7
+
+val fix = 29
+
+
+fun main(args:Array<String>){
+    println(num)
+    println(hello)
+    println(point)
+    println(fix)
+}
