509a23a49c44e40afd5e5dd58c2208c4da70612b
stmdals888 <45705721+smin9408@users.noreply.github.com>
2022-01-31 20:14:31 +0900

    Kotlin Test2

---
+++ app/src/main/java/com/example/kotlin_basic/kotlin_note/13.Test(2).kt
@@ -0,0 +1,18 @@
+package com.example.kotlin_basic.kotlin_note
+
+fun main(args: Array<String>) {
+
+}
+
+// 1.사칙 연산을 수행할 수 있는 클래스
+
+// 2.은행 계좌 만들기
+//   - 계좌 생성 기능 (이름, 생년월일)
+//   - 잔고를 확인하는 기능
+//   - 출금 기능
+//   - 예금 기능
+
+// 3.TV 클래스
+//   - on/off 기능
+//   - 채널을 돌리는 기능
+//   - 초기 채널은 (S사 M사 K사 3개)
