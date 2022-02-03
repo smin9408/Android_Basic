91316a1a46feecd60f642d17b01d86cc2de22de9
stmdals888 <45705721+smin9408@users.noreply.github.com>
2022-02-03 21:11:01 +0900

    Kotlin Variable Range -C

--- app/src/main/java/com/example/kotlin_basic/kotlin_note/14.Variable_range.kt
+++ app/src/main/java/com/example/kotlin_basic/kotlin_note/14.Variable_range.kt
@@ -1,7 +1,28 @@
 package com.example.kotlin_basic.kotlin_note
 
+// 변수의 접근 범위
+// 1.전역 변수
+// 2.지역 변수
+
+// 접근 범위는 가능한 작은범위에서 선언한다
+
+var number100: Int = 10
+
 fun main(args: Array<String>) {
 
+    val test = Test("승민")
+    test.name
+    println(test.name)
+    println(number100)
+
+    test.testFun()
+    println(test.name)
+    println(number100)
 }
 
-// Holiday but Commit because 1day 1commit
+class Test(var name: String) {
+    fun testFun() {
+        name = "홍길동"
+        number100 = 100
+    }
+}
